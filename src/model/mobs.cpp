d079ee183b0f330261d4ed29c4c2cf689951af7c
Radmila Chinaeva <milamnv@gmail.com>
2023-04-30 02:33:24 +0300

    Add mobs constructors and initializers

--- src/model/mobs.cpp
+++ src/model/mobs.cpp
@@ -1,10 +1,39 @@
 #include "mobs.h"
 
+Mob::Mob(Map& map): map(map) {}
+
+void Mob::Init() {
+    Cell cell = GetSpawnCell();
+
+    position = map.GetPosition(cell);
+    direction = GetSpawnDirection();
+    speed = GetInitialSpeed();
+}
+
+Direction Mob::GetSpawnDirection() {
+    return Direction::NONE;
+}
+
+int Mob::GetInitialSpeed() {
+    return 1;
+}
+
+Cell Pacman::GetSpawnCell() {
+    return {19, 3};
+}
+
+Cell Ghost::GetSpawnCell() {
+    return {19, 12};
+}
+
 void Mob::UpdatePosition() {
     return;
 }
 
 void Ghost::UpdatePosition() {
-    return;
-    // Mob::UpdatePosition();
+    Mob::UpdatePosition();
 }
+
+Pacman::Pacman(Map& map): Mob(map) {}
+
+Ghost::Ghost(Map& map): Mob(map) {}
--- src/model/mobs.h
+++ src/model/mobs.h
@@ -1,15 +1,33 @@
 #pragma once
+#include <map.h>
 
 class Mob {
+    Position position;
+    Direction direction;
+    int speed;
+    // DirectionChooser& direction_chooser;
+    Map& map;
+
+    virtual Cell GetSpawnCell() = 0;
+    static Direction GetSpawnDirection();
+    static int GetInitialSpeed();
+
 public:
+    Mob(Map&);
+    void Init();
+
     void UpdatePosition();
 };
 
 class Pacman : public Mob {
-
+    Cell GetSpawnCell();
+public:
+    Pacman(Map&);
 };
 
 class Ghost : public Mob {
+    Cell GetSpawnCell();
 public:
     void UpdatePosition();
+    Ghost(Map&);
 };
