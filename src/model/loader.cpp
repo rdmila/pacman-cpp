1d14955c3463337b073f146b6cc84c7cd2dfd7f3
Radmila Chinaeva <milamnv@gmail.com>
2023-05-04 15:51:53 +0300

    Fix map.txt path in loader.cpp

--- src/model/loader.cpp
+++ src/model/loader.cpp
@@ -2,12 +2,13 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <filesystem>
 
 void Loader::Load(Map& map) {
-    std::ifstream map_file("content/map.txt");
+    std::ifstream map_file("../../content/map.txt"); // TODO: make better path
     std::string line;
+//    std::cout << std::filesystem::current_path() << std::endl;
     std::getline(map_file, line);
-    std::cout << line << std::endl;
     int height = std::stoi(line);
 
     std::getline(map_file, line);
