85b311252ce7961d641d11b20ae2c45652c6c00c
Radmila Chinaeva <milamnv@gmail.com>
2023-05-06 19:38:52 +0300

    Fix fps

--- src/game.h
+++ src/game.h
@@ -8,7 +8,7 @@ using namespace std::chrono_literals;
 
 class Game {
     using clock_type = std::chrono::steady_clock;
-    const static std::chrono::nanoseconds constexpr update_interval = 1'000'000'000ns;
+    constexpr static std::chrono::nanoseconds update_interval = 17'000'000ns;
 
     std::chrono::time_point<clock_type> last_run_time;
     Model model;
