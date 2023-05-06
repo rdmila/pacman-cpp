cc41bea2cbe8b712bdb868cfe0094272ae73e04d
Radmila Chinaeva <milamnv@gmail.com>
2023-05-06 19:41:16 +0300

    Beautiful logger

--- src/view/view.cpp
+++ src/view/view.cpp
@@ -5,10 +5,12 @@ ConsoleLogger::ConsoleLogger(Model &model) : View(model) {}
 
 void ConsoleLogger::update() {
     Position pacman_pos = model.pacman.GetPosition();
-    std::cout << pacman_pos.edge.first.x << " " << pacman_pos.edge.first.y << std::endl;
+    std::cout << "Pacman position: " << pacman_pos.edge.first.x << " " << pacman_pos.edge.first.y << std::endl;
+    int i = 0;
     for (auto &g: model.ghosts) {
         Position pos = g->GetPosition();
-        std::cout << pos.edge.first.x << " " << pos.edge.first.y << std::endl;
+        std::cout << "Ghost " << i << " position: " << pos.edge.first.x << " " << pos.edge.first.y << std::endl;
+        ++i;
     }
 }
 
